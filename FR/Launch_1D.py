ac8589dd8f002dc626cca3db2c137512579581cb
GhislainRaze <gharz@hotmail.com>
2017-02-03 17:02:47 +0100

    Approche FR
    Ajout des polynomes pour l'approche FR

---
+++ FR/Launch_1D.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' author: R. Pile & A. Schmalzried '''
+''' 19/11/2016 '''
+''' Launch_1D.py : Script for DFR method '''
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import math as m
+from scipy import integrate
+import FR_1D
+
+### Parameters
+
+# SD order
+p = 6
+
+#Number of cells
+N = 100  
+
+# Stability criterion : CFL 
+CFL = 0.5                                  # La condition CFL semble plus restrictive que CFL < 1 (GR)
+
+#Final time
+Tfin = 0.05
+
+# Domain Length
+L = 1.0
+
+# Velocity c (m/s) and diffusion D (m^2/s)
+c = 10.
+D = 0. #2.5e-3    
+
+ # Penalizing parameter
+tau = 0.
+
+#Initialization 
+# init='Gauss' --> Gaussian law 
+#init='Constant' --> Constant law 
+# init = 'Triangle' --> Linear Tipi law
+#init='RectErf'--> Rectangular step with erf function
+
+init='Gauss'
+
+
+# Gradient for initialize the erf 
+grad_init=10**(-12)
+
+#boundary conditions on the left side bcondL and the right side bcondR
+#bcond*=0 for a Dirichlet BC
+#bcond*=1 for a periodic BC
+bcond = 1;
+# If Dirichlet conditions specify values
+yL=0.
+yR=0.
+
+#Cell spacing :
+# cellmask = 'Regular' -> evenly spaced cells
+# cellmask = 'Irregular' -> unevenly customisable cell spacing
+cellmask = 'Irregular'
+
+### Computing solution
+x0, sol0, x, sol, niter = FR_1D.main(p,CFL,Tfin,c,D,init,grad_init,bcond,yL*(1-bcond),yR*(1-bcond),cellmask,N,L,tau)
+
+
+
+# Write initialization
+file=open("results_init.txt","w")
+for i in range(len(x0)):
+    file.write(str(x0[i])+",")
+    file.write(str(sol0[i]) + "\n")
+
+file.close()
+
+# Write results, need to modify filename 
+file = open("results_CFL="+str(CFL)+"_p="+str(p)+".txt", "w")   
+yth=np.zeros(len(x))
+for i in range(len(x)):
+    if init=='Gauss':
+        if bcond == 0:
+            yth[i]=1/m.sqrt((4*D*Tfin/0.0001)+1)*m.exp(-(x[i]-c*Tfin+0.1)**2/(4*D*Tfin+0.0001))
+        else:
+            yth[i]=1/m.sqrt((4*D*Tfin/0.0001)+1)*m.exp(-(np.mod(x[i]-c*Tfin+0.1+L/2,L)-L/2)**2/(4*D*Tfin+0.0001))
+    elif init=='Constant':
+        yth[i] = 10. 
+    elif init=='RectErft':
+        if method==0:
+            yth[i]=(1-m.erf((x[i]-c*(Tfin+grad_init))))/2
+        else:
+            yth[i]=(1-m.erf((x[i]-c*(Tfin+grad_init))/(2*m.sqrt(D*(Tfin+grad_init)))))/2
+    elif init=='Sine':
+            yth[i]=m.cos(2*2*np.pi*(x[i]-c*Tfin)) 
+
+
+file.close()
+
+#print 'Solution initiale'
+#print sol0
+#print 'Solution calculee'
+#print sol
+
+#for i in range(len(x)):
+#    print sol0[i]
+
+### Solution plotting
+plt.plot(x0,sol0,'k-')
+plt.plot(x,sol, 'r-')
+plt.plot(x,yth, 'b--')
+plt.legend(['Initial', 'Convected','Theoretical'])
+plt.show()
+
+
---
+++ FR/polynomials.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 30 16:10:19 2016
+
+@author: Ghislain Raze
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#############################################################################################################
+#############################################################################################################
+##                                                                                                         ##
+##                                                                                                         ##
+##                                          POLYNOMIALS.PY                                                 ##
+##                       Polynomials used for the Flux Reconstruction approach                             ##
+##                                                                                                         ##
+##                                                                                                         ##
+#############################################################################################################
+#############################################################################################################
+
+
+
+
+# Returns the coefficients of the Legendre polynomials of given order
+# The coefficients are arranged in a matrix such that
+#       The rows contain the Legendre polynomial coefficients of the same order
+#       The columns correspond to the coefficient of a given power of x
+def legendreCoef(order):
+    
+    if order < 0 or order-int(order)!=0 :
+        return 0.
+    elif order == 0:
+        return 1.
+    elif order == 1:
+        return np.array([1,0],[0,1])
+    else:
+        # Construction from recurrence relation        
+        leg = np.zeros([order+1,order+1])
+        leg[0][0] = 1
+        leg[1][1] = 1
+        for k in range(2,order+1):
+            for l in range(k+1):
+                leg[k][l] = (2*k-1.)/k * leg[k-1][l-1] - (k-1.)/k * leg[k-2][l]
+        return leg  
+
+
+# Returns the Radau polynomials evaluated at x
+# The returned matrix is such that the lines corresponds to the Legendre polynomials of the same order evaluated at x
+# startOrder can be used to only compute the order-startOrder+1 last polynomials
+def legendre(x,order,startOrder = 0):
+
+    y = np.zeros([order+1-startOrder,len(x)])
+    leg = legendreCoef(order)
+    for i in range(order+1):
+        for j in range(startOrder,order+1):
+            y[j-startOrder] = y[j-startOrder] + leg[j,i]*x**i
+
+    return y
+
+
+# Returns the coefficients of the Radau polynomials of given order 
+# The coefficients are arranged in a matrix such that
+#       The rows contain the Radau polynomial coefficients of the same order
+#       The columns correspond to the coefficient of a given power of x       
+def radauCoef(order):
+    leg = legendreCoef(order)
+    rad = np.zeros([order+1,order+1])
+    for k in range(1,order+1):
+        rad[k] = 0.5*(-1.)**k * (leg[k] - leg[k-1])
+        
+    return rad
+ 
+
+# Returns the Radau polynomials evaluated at x
+# The returned matrix is such that the lines corresponds to the Radau polynomials of the same order evaluated at x
+# startOrder can be used to only compute the order-startOrder+1 last polynomials
+def radau(x,order,startOrder = 0):
+
+    y = np.zeros([order+1-startOrder,len(x)])
+    rad = radauCoef(order)
+    for i in range(order+1):
+        for j in range(startOrder,order+1):
+            y[j-startOrder] = y[j-startOrder] + rad[j,i]*x**i
+
+    return y
+
+
+# Returns the coefficients of the Lobatto polynomials of given order
+# The coefficients are arranged in a matrix such that
+#       The rows contain the Lobatto polynomial coefficients of the same order
+#       The columns correspond to the coefficient of a given power of x    
+def lobattoCoef(order):
+    leg = legendreCoef(order)
+    lob = np.zeros([order+1,order+1])
+    for k in range(2,order+1):
+        lob[k] = (leg[k] - leg[k-2])
+        
+    return lob
+
+
+# Returns the Lobatto polynomials evaluated at x
+# The returned matrix is such that the lines corresponds to the lobatto polynomials of the same order evaluated at x
+# startOrder can be used to only compute the order-startOrder+1 last polynomials  
+def lobatto(x,order,startOrder = 0):
+
+    y = np.zeros([order+1-startOrder,len(x)])
+    lob = lobattoCoef(order)
+    for i in range(order+1):
+        for j in range(startOrder,order+1):
+            y[j-startOrder] = y[j-startOrder] + lob[j,i]*x**i
+
+    return y    
+
+
+# Returns the values of  polynomials of given order    
+def lagrange(x,xi,yi):
+    
+    lag = np.ones([len(xi),len(x)])     
+    dx = np.ones([len(xi),len(x)])
+    
+    for i in range(len(dx)):
+        dx[i] = x - xi[i]
+        
+    for i in range(len(lag)):
+        for j in range(len(lag)):
+            if i != j:
+                lag[i] = lag[i]*dx[j]/(xi[i]-xi[j])
+        lag[i] = lag[i]*yi[i]
+    
+    y = np.sum(lag,1)
+    
+    return y
+    
+
+# Returns the g2 function evaluated at x
+def g2(x,p):
+    rad = radau(x,p,p-1)
+    y = (p-1)/(2*p-1.0) *rad[1] + p/(2*p-1.0)*rad[0]
+    return y
+
+
+# Returns the gl and gr functions evaluated at x
+def glgr(x,p):
+    leg = legendre(x,p,p)
+    gl = 0.5*(-1)**p * (1-x) * leg[0]
+    gr = 0.5 * (1+x) * leg[0]
+    return gl, gr
+
+
+# Energy-preserving schemes ?
+
+
+
+
+
+
+# To check the correction functions
+
+#p = 4
+#x = np.linspace(-1,1,100)
+#y = g2(x,p)
+
+#plt.figure
+#plt.plot(x,y)
+#plt.show()
+
+
+#gl,gr = glgr(x,p)
+#plt.figure
+#plt.plot(x,gl)
+#plt.plot(x,gr)
+#plt.show()
